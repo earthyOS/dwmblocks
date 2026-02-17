202eb1f22b0190067cc1173f987e6500f45d5acc
Ambrose <me@librelife.org>
2026-02-17 01:20:40 -0500

    Removed unused section

--- blocks.h
+++ blocks.h
@@ -13,7 +13,6 @@ static const Block blocks[] = {
 	{"CPU: ",	"sb-cpuusage",			1,		0},
 	{"Mem: ",	"sb-memory",			1,		0},
 	{"Temp: ",	"sb-cputemp",		1,		0},
-	//{"IP: ",	"sb-network",			1,		0},
 	//{"Bat: ",	"sb-battery",			60,		0},
 	{"",		"date '+%b %d, %I:%M %p'",	1,		0},
 };
